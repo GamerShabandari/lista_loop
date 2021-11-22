86cff4fdca2d1e1dba4f1170890ec642e51f4e54
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2021-11-22 19:39:17 +0100

    klar med uppgift v1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hejehej");
+console.log("hejehejdå");
+
+minLista = document.getElementById("minLista");
+
+var minaHobbyn = ["koda" , "spela" , "fotboll" , "träna" , "chilla"];
+
+for (i=0; i<minaHobbyn.length; i++){
+    console.log(minaHobbyn[i]);
+    minLista.insertAdjacentHTML('beforeend', '<li>'+ minaHobbyn[i] +'</li>');
+    
+};
